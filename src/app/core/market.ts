7cd7c1b7e4a586f1da76c03e59ccfd6b2a4c3f18
yayasimo454-boop <yayasimo454@gmail.com>
2026-09-08 11:46:33 +0100

    Fix: import environment correct dans market.ts + build en mode production

--- src/app/core/market.ts
+++ src/app/core/market.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, interval, switchMap, shareReplay, startWith, catchError, of } from 'rxjs';
 import { environment } from '../../environments/environment';
+
 export interface Asset {
   id: number;
   symbol: string;
